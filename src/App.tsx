193747f6cdfe0d48d343627885b95fb77a202ee1
BRNOHXZ <gkvznvtj@icloud.com>
2021-08-28 17:34:56 +0300

    Header and image rm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Counter from './components/Counter';
+import './App.css'
 function App() {
   return (
     <div className="App">
-
+      <Counter/>
     </div>
   );
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { blue } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SettingsPicker from './SettingPicker';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            minWidth:345,
+            maxWidth: 345,
+        },
+        expand: {
+            transform: 'rotate(0deg)',
+            marginLeft: 'auto',
+            transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest,
+            }),
+        },
+        expandOpen: {
+            transform: 'rotate(180deg)',
+        },
+    }),
+);
+
+export default function Counter() {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+    const [counter, setCounter] = React.useState(0)
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <h1>{counter}</h1>
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+                <IconButton
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <SettingsPicker/>
+                </CardContent>
+            </Collapse>
+        </Card>
+    );
+}
